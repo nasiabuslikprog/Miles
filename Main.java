f4234b94bf1c3d738f14aabe3c8a2cede4b00b2a
nasiabuslikprog <nasiabuslikprog@yandex.ru>
2022-10-11 10:24:22 +0300

    initional commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int price = 4000;
+        int onemiles = 20;
+         int bonus = ( price/ onemiles);
+        System.out.println(bonus);
+    }
+}
